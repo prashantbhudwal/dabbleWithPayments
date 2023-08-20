0306bb5d5c966eb2fa038a1b29fe72f3bf915eb3
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-08-20 17:49:24 +0530

    Add plan component

--- app/components/Plans.tsx
+++ app/components/Plans.tsx
@@ -1,10 +1,40 @@
 "use client";
+import { GetPricesReturnType } from "../utils/stripe";
+import axios from "axios";
+import { redirect } from "next/navigation";
 
-export default function Plans() {
-    
+type PlansProps = {
+  prices: GetPricesReturnType["data"]; // An array of Stripe.Price objects
+};
+
+const checkout = async (priceId: string) => {
+  const res = await axios.post("/api/stripe/checkout", {
+    priceId,
+  });
+  console.log(res.data);
+  window.location.assign(res.data);
+};
+
+const Plans: React.FC<PlansProps> = ({ prices }) => {
   return (
-    <div>
-      <h1>Client Component</h1>
+    <div className=" flex gap-2">
+      {prices.map((price) => (
+        <div key={price.id} className=" bg-slate-500">
+          <h2>{price.nickname || "No nickname"}</h2>
+          <p>
+            {price.unit_amount ? price.unit_amount / 100 : 0}{" "}
+            {price.currency.toUpperCase()}
+          </p>
+          <button
+            className="bg-zinc-200 p-2"
+            onClick={() => checkout(price.id)}
+          >
+            Buy now
+          </button>
+        </div>
+      ))}
     </div>
   );
-}
+};
+
+export default Plans;
--- app/page.tsx
+++ app/page.tsx
@@ -4,19 +4,9 @@ export const revalidate = 3600; // revalidate the data at most every hour
 
 export default async function Page() {
   const prices = await getPrices();
-  console.log("prices", prices);
   return (
     <div>
-      {prices.data.map((price) => (
-        <div key={price.id}>
-          <h2>{price.nickname || "No nickname"}</h2>
-          <p>
-            {price.unit_amount ? price.unit_amount / 100 : 0}{" "}
-            {price.currency.toUpperCase()}
-          </p>
-        </div>
-      ))}
-      <Plans />
+      <Plans prices={prices.data} />
     </div>
   );
 }
