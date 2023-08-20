85dac9744ea0d50b5a023a4781d257b0a1dc17ef
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-08-20 14:45:31 +0530

    Add types for prices

---
+++ types.ts
@@ -0,0 +1,25 @@
+export type SPrice = {
+  id: string;
+  object: string;
+  active: boolean;
+  billing_scheme: string;
+  created: number;
+  currency: string;
+  custom_unit_amount: null;
+  livemode: boolean;
+  lookup_key: null;
+  metadata: Metadata;
+  nickname: null;
+  product: string;
+  recurring: null;
+  tax_behavior: string;
+  tiers_mode: null;
+  transform_quantity: null;
+  type: string;
+  unit_amount: number;
+  unit_amount_decimal: string;
+};
+
+export type Metadata = {
+  [key: string]: string;
+};
