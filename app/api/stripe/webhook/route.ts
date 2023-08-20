3906b0c1b30ce4ecdeae617ae0f34964f4147585
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-08-20 21:14:44 +0530

    Add webhook

---
+++ app/api/stripe/webhook/route.ts
@@ -0,0 +1,37 @@
+import { NextRequest, NextResponse } from "next/server";
+import { checkout } from "@/app/utils/stripe";
+import Stripe from "stripe";
+import { cache } from "react";
+import { headers } from "next/headers";
+import { stripe } from "@/app/utils/stripe";
+import { buffer } from "micro";
+import { error } from "console";
+
+
+export async function POST(req: NextRequest) {
+  const sig = req.headers.get("stripe-signature");
+  const endpointSecret = process.env.STRIPE_WEBHOOK_SECRET;
+  const buf = await req.text();
+  let event: Stripe.Event;
+
+  try {
+    if (!sig || !endpointSecret) return;
+    event = stripe.webhooks.constructEvent(buf, sig, endpointSecret);
+  } catch (error) {
+    console.log(error);
+    return NextResponse.error();
+  }
+
+  console.log(event);
+  switch (event.type) {
+    case "payment_intent.succeeded":
+      const paymentIntentSucceeded = event.data.object as Stripe.PaymentIntent;
+      console.log("data", paymentIntentSucceeded.metadata);
+      // Then define and call a function to handle the event payment_intent.succeeded
+      break;
+    // ... handle other event types
+    default:
+      console.log(`Unhandled event type ${event.type}`);
+  }
+  return NextResponse.json({ received: true }, { status: 200 });
+}
